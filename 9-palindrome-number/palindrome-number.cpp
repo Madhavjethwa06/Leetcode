9ce428d15fee3c8044d418e137fa4833629ba264
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-07-10 21:58:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (68.17%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        int temp=x;
+        long reverse=0;
+
+        while(x>0){
+            reverse=(reverse*10)+(x%10);
+            x/=10;
+        }
+        return reverse==temp;
+    }
+};
