2cbf110c8f8da3ed10d42894d3e8add60a888ce7
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-07-31 23:08:06 +0530

    Time: 8 ms (6.55%) | Memory: 10.1 MB (42.95%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        string cleaned = "";
+        int left = 0, right = s.size()-1;
 
-        // Step 1: Clean the string (keep only alphanumeric & convert to lowercase)
-        for (char c : s) {
-            if (isalnum(c)) {
-                cleaned += tolower(c);
-            }
+        while(left<right){
+            while(left<right && !isalnum(s[left])) left++;
+
+            while(left<right && !isalnum(s[right])) right--;
+
+            if(tolower(s[left]) != tolower(s[right]))
+            return false;
+
+            left++;
+            right--;
         }
-
-        // Step 2: Check if cleaned string is a palindrome
-        int left = 0, right = cleaned.length() - 1;
-        while (left < right) {
-            if (cleaned[left] != cleaned[right]) {
-                return false;
-            }
-            ++left;
-            --right;
-        }
-
         return true;
+        
     }
 };
