a59c7ffb164ae410a3c37960f9758515b997565a
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-07-31 23:02:02 +0530

    Time: 6 ms (7.48%) | Memory: 10.9 MB (6.01%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned="";
+        for (char c : s) {
+            if (isalnum(c)) {
+                cleaned += tolower(c);
+            }
+        }
+       
+     
+        string temp=cleaned;
+         string reversed = cleaned;
+        reverse(reversed.begin(), reversed.end());
+
+        // Compare cleaned and reversed strings
+        return cleaned == reversed;
+
+        
+    }
+};
