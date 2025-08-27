e50db50a3fd888e40e3c26e3f4527406fd5a120d
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-08-27 15:40:02 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (78.93%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.cpp
@@ -1,16 +1,28 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
-        sort(nums1.begin(),nums1.end());
-        sort(nums2.begin(),nums2.end());
-        set<int>ans;
-        
-        for(int i=0;i<nums1.size();i++){
-            for(int j=0;j<nums2.size();j++){
-                if(nums1[i]==nums2[j])ans.insert(nums1[i]);
+        sort(nums1.begin(), nums1.end());
+        sort(nums2.begin(), nums2.end());
+
+        vector<int> result;
+        int i = 0, j = 0;
+
+        while (i < nums1.size() && j < nums2.size()) {
+            if (nums1[i] == nums2[j]) {
+                if (result.empty() || result.back() != nums1[i]) {
+                    result.push_back(nums1[i]); // avoid duplicates
+                }
+                i++;
+                j++;
+            }
+            else if (nums1[i] < nums2[j]) {
+                i++;
+            }
+            else {
+                j++;
             }
         }
-        vector<int>ans1(ans.begin(),ans.end());
-        return ans1;
+
+        return result;
     }
 };
