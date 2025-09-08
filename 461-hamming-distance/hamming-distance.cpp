21bcd3bd17708184080a02b22b075e51c97ab3cd
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-09-09 00:16:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (34.80%) - LeetSync

---
+++ 461-hamming-distance/hamming-distance.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int count=0;
+        for(int i=0;i<32;i++){
+            if(x%2!=y%2) count++;
+            x/=2;
+            y/=2;
+        }
+        return count;
+    }
+};
