e07f0a135ccd2d892a861244aaf8fe6fdc76552d
Madhavjethwa06 <madhavjethwa06@gmail.com>
2025-12-23 14:19:31 +0530

    Time: 9 ms (58.62%) | Memory: 32.2 MB (17.40%) - LeetSync

--- 561-array-partition/array-partition.cpp
+++ 561-array-partition/array-partition.cpp
@@ -4,7 +4,7 @@ public:
         sort(nums.begin(),nums.end());
         int ans=0;
         for(int i=0;i<nums.size();i+=2){
-            ans+=min(nums[i],nums[i+1]);
+            ans+=nums[i];
             
         }
         return ans;
