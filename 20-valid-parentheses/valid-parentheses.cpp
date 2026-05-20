3d0005ed6da48352e899adad4a47461071088524
Madhav Jethwa <madhavjethwa06@gmail.com>
2026-05-20 17:52:03 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (37.62%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -2,25 +2,32 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char>a;
-        for (char c : s) {
-            if(c=='(' || c=='{' || c=='['){
+        for(char c:s){
+            if(c=='('|| c=='[' || c=='{'){
                 a.push(c);
-            }
-            else{
+            }else{
                 if(a.empty()){
                     return false;
-                }else if(c==')' && a.top()=='(' ||
-                        c=='}' && a.top()=='{'||
-                        c==']' && a.top()=='['){
-                            a.pop();
-                        }
-                else{
+                }
+            char c1=a.top();
+            a.pop();
+           
+            if(c==')'){
+                if(c1!='('){
                     return false;
                 }
-
             }
-
-        }
-        return a.empty();
+             if(c=='}'){
+                if(c1!='{'){
+                    return false;
+                }
+            } if(c==']'){
+                if(c1!='['){
+                    return false;
+                }
+            }
+            }
+        }   
+        return a.empty(); 
     }
 };
